a072a435930307cf94a14c650946e6a31c9cc0f7
user <user@deb12-dev.2bid.net.dev>
2025-09-04 17:23:31 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 HOST=root@192.168.21.57
-#ssh $HOST "docker rm heating && docker image rm heating:latest"
+ssh $HOST "docker rm heating && docker image rm heating:latest"
 
-#set -e
-#docker compose build
-#docker save heating:latest > heating.img.tar
+set -e
+docker compose build
+docker save heating:latest > heating.img.tar
 
-#cat heating.img.tar | ssh $HOST "docker load"
+cat heating.img.tar | ssh $HOST "docker load"
 
 DOCKER_HOST="ssh://$HOST" docker compose up
